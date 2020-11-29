213a9782ed2334f200ed379281045a40c93cec02
jbronxxx <jbronxxx@gmail.com>
2020-11-30 00:23:47 +0500

    Added FileLineToList method

--- DatabaseParser/Controllers/Parser.cs
+++ DatabaseParser/Controllers/Parser.cs
@@ -1,8 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
 using System.IO;
 using Microsoft.AspNetCore.Hosting;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DatabaseParser.Models
 {
@@ -15,7 +16,7 @@ namespace DatabaseParser.Models
             _env = env;
         }
 
-        // POST: Upload database from file
+        // POST: Upload .csv file to wwwroot/files
         [HttpPost]
         public IActionResult DataBaseUpload(IFormFile file)
         {
@@ -28,7 +29,30 @@ namespace DatabaseParser.Models
                     file.CopyTo(fileStream);
                 }
             }
+
             return RedirectToAction("Index","Employee");
+        }
+
+        private List<string> GetFileLinesToList(IFormFile fName)
+        {
+            List<string> fileLines = new List<string>();
+
+            var fileName = $"{Directory.GetCurrentDirectory()}{@"\wwwroot\files"}" + "\\" + fName;
+
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
+            using (var fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+            {
+                using(var streamReader = new StreamReader(fileName))
+                {
+                    while (!streamReader.EndOfStream)
+                    {
+                        fileLines.Add(streamReader.ReadLine());
+                    }
+                }
+            }
+
+            return fileLines;
         }
     }
 }
