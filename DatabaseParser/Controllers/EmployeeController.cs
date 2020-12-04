b6c647538f3694049c9f6d06f608eb664f6fd059
jbronxxx <jbronxxx@gmail.com>
2020-12-04 17:46:24 +0500

    Fixed sorting bug

--- DatabaseParser/Controllers/EmployeeController.cs
+++ DatabaseParser/Controllers/EmployeeController.cs
@@ -17,68 +17,68 @@ namespace DatabaseParser.Controllers
         }
 
         // GET: Employee
-        public async Task<IActionResult> Index(string sortOrder, string searchString)
+        public async Task<IActionResult> Index(string searchString, SortOrder sortOrder = SortOrder.SurNameAsc)
         {
-            ViewData["PayrollNumberSort"] = String.IsNullOrEmpty(sortOrder) ? "payrollNumberDesc" : "";
-            ViewData["ForeNameSort"]      = String.IsNullOrEmpty(sortOrder) ? "foreNameDesc" : "";
-            ViewData["SurNameSort"]       = String.IsNullOrEmpty(sortOrder) ? "surNameDesc" : "";
-            ViewData["DateOfBirthSort"]   = String.IsNullOrEmpty(sortOrder) ? "dateOfBirthDesc" : "";
-            ViewData["TelephoneSort"]     = String.IsNullOrEmpty(sortOrder) ? "telephoneDesc" : "";
-            ViewData["MobileSort"]        = String.IsNullOrEmpty(sortOrder) ? "mobileDesc" : "";
-            ViewData["AdressSort"]        = String.IsNullOrEmpty(sortOrder) ? "adressDesc" : "";
-            ViewData["Adress2Sort"]       = String.IsNullOrEmpty(sortOrder) ? "adress2Desc" : "";
-            ViewData["PostCodeSort"]      = String.IsNullOrEmpty(sortOrder) ? "postCodeDesc" : "";
-            ViewData["EmailHomeSort"]     = String.IsNullOrEmpty(sortOrder) ? "emailHomeDesc" : "";
-            ViewData["StartDateSort"]     = String.IsNullOrEmpty(sortOrder) ? "startDateDesc" : "";
+            ViewData["PayrollNumberSort"] = sortOrder == SortOrder.PayrollNumberAsc ? SortOrder.PayrollNumberDesc : SortOrder.PayrollNumberAsc;
+            ViewData["ForeNameSort"]      = sortOrder == SortOrder.ForeNameAsc ? SortOrder.ForeNameDesc : SortOrder.ForeNameAsc;
+            ViewData["SurNameSort"]       = sortOrder == SortOrder.SurNameAsc ? SortOrder.SurNameDesc : SortOrder.SurNameAsc;
+            ViewData["DateOfBirthSort"]   = sortOrder == SortOrder.DateOfBirthAsc ? SortOrder.DateOfBirthDesc : SortOrder.DateOfBirthAsc;
+            ViewData["TelephoneSort"]     = sortOrder == SortOrder.TelephoneAsc ? SortOrder.TelephoneDesc : SortOrder.TelephoneAsc;
+            ViewData["MobileSort"]        = sortOrder == SortOrder.MobileAsc ? SortOrder.MobileDesc : SortOrder.MobileAsc;
+            ViewData["AdressSort"]        = sortOrder == SortOrder.AdressAsc ? SortOrder.AdressDesc : SortOrder.AdressAsc;
+            ViewData["Adress2Sort"]       = sortOrder == SortOrder.Adress2Asc ? SortOrder.Adress2Desc : SortOrder.Adress2Asc;
+            ViewData["PostCodeSort"]      = sortOrder == SortOrder.PostCodeAsc ? SortOrder.PostCodeDesc : SortOrder.PostCodeAsc;
+            ViewData["EmailHomeSort"]     = sortOrder == SortOrder.EmailHomeAsc ? SortOrder.EmailHomeDesc : SortOrder.EmailHomeAsc;
+            ViewData["StartDateSort"]     = sortOrder == SortOrder.StartDateAsc ? SortOrder.StartDateDesc : SortOrder.StartDateAsc;
 
             ViewData["SearchFilter"]      = searchString;
 
-            var employees = from s in _context.Employees select s;
+            IQueryable<Employee> employees = _context.Employees;
 
+            // Searching by name
             if (!String.IsNullOrEmpty(searchString))
             {
                 employees = employees.Where(s => s.SurName.Contains(searchString) || s.ForeNames.Contains(searchString));
             }
 
-            switch (sortOrder)
+            // Sorting table
+            employees = sortOrder switch
             {
-                case "payrollNumberDesc":
-                    employees = employees.OrderByDescending(e => e.PayrollNumber);
-                    break;
-                case "foreNameDesc":
-                    employees = employees.OrderByDescending(e => e.ForeNames);
-                    break;
-                case "surNameDesc":
-                    employees = employees.OrderByDescending(e => e.SurName);
-                    break;
-                case "dateOfBirthDesc":
-                    employees = employees.OrderByDescending(e => e.DateOfBirth);
-                    break;
-                case "telephoneDesc":
-                    employees = employees.OrderByDescending(e => e.Telephone);
-                    break;
-                case "mobileDesc":
-                    employees = employees.OrderByDescending(e => e.Mobile);
-                    break;
-                case "adressDesc":
-                    employees = employees.OrderByDescending(e => e.Adress);
-                    break;
-                case "adress2Desc":
-                    employees = employees.OrderByDescending(e => e.Adress2);
-                    break;
-                case "postCodeDesc":
-                    employees = employees.OrderByDescending(e => e.PostCode);
-                    break;
-                case "emailHomeDesc":
-                    employees = employees.OrderByDescending(e => e.EmailHome);
-                    break;
-                case "startDateDesc":
-                    employees = employees.OrderByDescending(e => e.StartDate);
-                    break;
-                default:
-                    //employees = employees.OrderBy(e => e.SurName);
-                    break;
-            }
+                SortOrder.PayrollNumberDesc => employees.OrderByDescending(s => s.PayrollNumber),
+                SortOrder.PayrollNumberAsc  => employees.OrderBy(s => s.PayrollNumber),
+
+                SortOrder.ForeNameDesc      => employees.OrderByDescending(s => s.ForeNames),
+                SortOrder.ForeNameAsc       => employees.OrderBy(s => s.ForeNames),
+
+                SortOrder.SurNameDesc       => employees.OrderByDescending(s => s.SurName),
+                SortOrder.SurNameAsc        => employees.OrderBy(s => s.SurName),
+
+                SortOrder.DateOfBirthDesc   => employees.OrderByDescending(s => s.DateOfBirth),
+                SortOrder.DateOfBirthAsc    => employees.OrderBy(s => s.DateOfBirth),
+
+                SortOrder.TelephoneDesc     => employees.OrderByDescending(s => s.Telephone),
+                SortOrder.TelephoneAsc      => employees.OrderBy(s => s.Telephone),
+
+                SortOrder.MobileDesc        => employees.OrderByDescending(s => s.Mobile),
+                SortOrder.MobileAsc         => employees.OrderBy(s => s.Mobile),
+
+                SortOrder.AdressDesc        => employees.OrderByDescending(s => s.Adress),
+                SortOrder.AdressAsc         => employees.OrderBy(s => s.Adress),
+
+                SortOrder.Adress2Desc       => employees.OrderByDescending(s => s.Adress2),
+                SortOrder.Adress2Asc        => employees.OrderBy(s => s.Adress2),
+
+                SortOrder.PostCodeDesc      => employees.OrderByDescending(s => s.PostCode),
+                SortOrder.PostCodeAsc       => employees.OrderBy(s => s.PostCode),
+
+                SortOrder.EmailHomeDesc     => employees.OrderByDescending(s => s.EmailHome),
+                SortOrder.EmailHomeAsc      => employees.OrderBy(s => s.EmailHome),
+
+                SortOrder.StartDateDesc     => employees.OrderByDescending(s => s.StartDate),
+                SortOrder.StartDateAsc      => employees.OrderBy(s => s.StartDate),
+
+                _                           => employees.OrderBy(s => s.SurName)
+            };
 
             return View(await employees.AsNoTracking().ToListAsync());
         }
---
+++ DatabaseParser/Models/SortOrder.cs
@@ -0,0 +1,38 @@
+﻿namespace DatabaseParser.Models
+{
+    public enum SortOrder
+    {
+        PayrollNumberDesc,
+        PayrollNumberAsc,
+
+        ForeNameDesc,
+        ForeNameAsc,
+
+        SurNameDesc,
+        SurNameAsc,
+
+        DateOfBirthDesc,
+        DateOfBirthAsc,
+
+        TelephoneDesc,
+        TelephoneAsc,
+
+        MobileDesc,
+        MobileAsc,
+
+        AdressDesc,
+        AdressAsc,
+
+        Adress2Desc,
+        Adress2Asc,
+
+        PostCodeDesc,
+        PostCodeAsc,
+
+        EmailHomeDesc,
+        EmailHomeAsc,
+
+        StartDateDesc,
+        StartDateAsc
+    }
+}
